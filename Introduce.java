23878611b9b089cac9e76548d3c4d2745cd82ad6
hyunaaaa <31638940+hyunaaaa@users.noreply.github.com>
2017-09-05 11:07:43 +0900

    1st assignment
    1st assignment

---
+++ Introduce.java
@@ -0,0 +1,10 @@
+
+public class Introduce {
+
+	public static void main(String args[]){
+		System.out.println("student id : 2016301022");
+		System.out.println("name : ±èÇö¾Æ");
+		System.out.println("phone number : 01029969901");
+		System.out.println("Github address : github.com/hyunaaaa");
+	}
+}
